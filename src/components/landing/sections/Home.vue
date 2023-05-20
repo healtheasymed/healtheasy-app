97937e193ad1e645a4426a8e85745dad85f77738
Health Easy <96735729+healtheasymed@users.noreply.github.com>
2023-05-20 17:59:25 -0300

    Update Home.vue

--- src/components/landing/sections/Home.vue
+++ src/components/landing/sections/Home.vue
@@ -7,9 +7,7 @@
         <h1>Sistema para clínicas e consultórios médicos</h1>
 
         <p>
-          Junte-se a médicos de todo o Brasil e transforme sua gestão com o
-          <b>Health Easy</b>. Da agenda online à gestão financeira, ganhe tempo
-          e lucratividade na rotina de sua clínica ou consultório médico.
+          Somos especializados em fornecer software de gestão para clínicas e consultórios médicos. Nossa solução de gestão de clínicas foi projetada para facilitar a vida dos médicos e suas equipes, melhorando a eficiência e a qualidade do atendimento ao paciente.
         </p>
 
         <CustomButton
