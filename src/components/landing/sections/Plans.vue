a08dc85b090e9e6cd6b8dd2aaa74706d924ec290
Health Easy <96735729+healtheasymed@users.noreply.github.com>
2023-05-22 17:53:39 -0300

    Update Plans.vue

--- src/components/landing/sections/Plans.vue
+++ src/components/landing/sections/Plans.vue
@@ -6,8 +6,14 @@
       <header class="title">
         <h2>Nossos <span>planos</span></h2>
         <p>
-          Lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements of a document or visual presentation.
+          Na Health Easy, temos planos flexíveis que são adaptados
+          às necessidades individuais de cada clínica ou consultório.
+          Oferecemos soluções personalizadas e eficazes para atender às suas demandas específicas.
+          Caso queira descobrir qual plano é o melhor para você ou sua empresa,
+          utilize nossa ferramenta de simulação
+          ou entre em contato conosco para saber mais sobre as nossas opções de planos.
+          Estamos prontos para ajudá-lo a encontrar a melhor solução
+          para a sua clínica ou consultório.
         </p>
       </header>
 
@@ -31,12 +37,7 @@ export default defineComponent({
 
 <style scoped>
 .plans {
-  background: linear-gradient(
-    180deg,
-    var(--light-green) 10%,
-    var(--white) 90%
-  );
-
+  background: linear-gradient(180deg, var(--light-green) 10%, var(--white) 90%);
   align-items: center;
   display: flex;
   flex-direction: column;
