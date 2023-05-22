6b82f4c7a5a45ca7043f7a6dc1a93939148738c0
Health Easy <96735729+healtheasymed@users.noreply.github.com>
2023-05-21 21:50:56 -0300

    Update Contact.vue
    atualização texto contato

--- src/components/landing/sections/Contact.vue
+++ src/components/landing/sections/Contact.vue
@@ -6,8 +6,7 @@
       <header class="title">
         <h2>Entre em <span>contato</span></h2>
         <p>
-          Lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements of a document or visual presentation.
+         Para entrar em contato conosco, basta preencher o formulário na aba "Contato" ou meio do número (85) 4042-6250
         </p>
 
         <div class="social-media">
